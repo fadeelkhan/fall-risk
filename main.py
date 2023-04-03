89ccb4f1b3ef66d040d1a59ad662f6a5a0c2b8af
Fadeel Sher Khan <fk13@rice.edu>
2023-04-02 21:41:41 -0500

    Updated main.py with real time prediction

--- main.py
+++ main.py
@@ -59,14 +59,14 @@ def create_window():
         [sg.Push(), sg.Image('cross.png', pad=0, enable_events=True, key='-CLOSE-')],
         [sg.VPush()],
         [sg.Text('', font='Young 50', key='-TIME-')],
-        [sg.Button('Start', button_color=('#FFFFFF', '#FF0000'), border_width=0, key='-STARTSTOP-')],
+        [sg.Button('Start', button_color=('#FFFFFF', '#FF0000'), border_width=0, key='-STARTSTOP-', size=(25,8))],
         [sg.VPush()]
     ]
 
     return sg.Window(
         'Physician Interface',
         layout,
-        size=(1550, 850),
+        size=(600, 600),
         no_titlebar=True,
         element_justification='center')
 
@@ -75,9 +75,8 @@ window = create_window()
 start_time = 0
 active = False
 lap_amount = 1
-n=0
+
 while True:
-    n+=1
     event, values = window.read(timeout=50)
     if event in (sg.WIN_CLOSED, '-CLOSE-'):
         break
@@ -105,17 +104,15 @@ while True:
             if bool(re.search(r'\d', packet.decode('utf', errors='ignore').rstrip('\n'))):
                 input_file = packet.decode('utf', errors='ignore').rstrip('\n')
                 input_file = np.array(list(map(float, re.findall('[-+]?[0-9]*\.?[0-9]+', input_file))))
-                for iterr in range(15):
+                for iterr in range(6):
                     new_packet = serialInst.readline()
                     new_lst = new_packet.decode('utf', errors='ignore').rstrip('\n')
                     new_lst = np.array(list(map(float, re.findall('[-+]?[0-9]*\.?[0-9]+', new_lst))))
                     input_file = input_file + new_lst
-                input_file = input_file / 15
-                # print('n=', n, 'iterr = ', iterr, 'and', input_file)
+                input_file = input_file / 6
         try:
             ### Predict Real Time
-            fall_vs_no_fall_predictions, types_of_activities_predictions = predictions.predict_using_existing_models(
-                input_file)
+            fall_vs_no_fall_predictions, types_of_activities_predictions = predictions.predict_using_existing_models(input_file)
             window['-TIME-'].update(types_of_activities_predictions[0])
         except:
             pass
